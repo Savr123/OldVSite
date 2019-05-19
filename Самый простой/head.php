50ac8b4cb937df766c5657639ae6747997cb7326
Savr123 <Sangievv@mail.ru>
2019-05-19 07:36:34 +0300

    secondCommit
    secondCommit
    I have add new header, getting started Form-registration and start to use PHP lang

---
+++ Самый простой/head.php
@@ -0,0 +1,57 @@
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" type="text/css" href="style/style.css">
+  <link rel="stylesheet" href="bootstrap-4.2.1-dist/css/bootstrap-grid.css">
+  <link rel="stylesheet" href="bootstrap-4.2.1-dist/css/bootstrap-reboot.css">
+  <link rel="stylesheet" href="bootstrap-4.2.1-dist/css/bootstrap.css">
+  <title>Music</title>
+</head>
+<body>
+  <div class="container-fluid">
+    <!--header-------------------------------------------------------------->
+  <header>
+    <nav class="navbar navbar-expand-lg spec-navbar">
+      <a class="navbar-brand" href="#"><span class="logo">MusicPlay</span></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <form class="form-inline my-2 my-lg-0 mr-auto">
+        <input class="form-control mr-sm-2" type="text" placeholder="Поиск">
+        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Поиск</button>
+      </form>
+
+      <div class="collapse navbar-collapse" id="navbarColor02">
+        <ul class="navbar-nav ml-auto mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link spec-nav-link spec-nav-link-anim" href="#">Главное <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link spec-nav-link spec-nav-link-anim" href="#">Рекомендации</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link spec-nav-link spec-nav-link-anim" href="#">Жанры</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link spec-nav-link spec-nav-link-anim" href="#">Радио</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="collapse navbar-collapse" id="spec-nav-personal">
+        <button type="button" name="login" class="btn spec-btn-log">Войти</button>
+        <!-- <a href="" id="spec-nav-link-profile" class="nav-link" style="background-image: url('images/profileImg.jpg')"></a>
+        <span class="spec-nav-link-anim"><a href="" id="spec-nav-link-liked" class="nav-link">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <path fill="none" fill-rule="evenodd" stroke="#222" stroke-width="2" d="M12 19.587c-1.116-1.104-3.173-3.126-3.917-3.87l-.317-.317c-.912-.914-1.69-1.712-2.335-2.393-1.867-1.975-1.894-4.871-.16-6.695 1.668-1.756 4.341-1.748 6 .017l.729.775.729-.775c1.659-1.765 4.331-1.773 6-.017 1.734 1.824 1.707 4.72-.16 6.695-.644.681-1.424 1.478-2.335 2.393l-.316.316c-.738.737-2.8 2.765-3.918 3.871z"/>
+          </svg>
+          Моя Музыка
+        </a></span> -->
+      </div>
+    </nav>
+
+    <!---->
+  </header>
+</body>
---
+++ Самый простой/login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ru" dir="ltr">
+  <head>
+    <title></title>
+    <meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width minimum-scale=0.1">
+  	<link rel="stylesheet" type="text/css" href="style/style.css">
+  	<link rel="stylesheet" href="bootstrap-4.2.1-dist/css/bootstrap-grid.css">
+  	<link rel="stylesheet" href="bootstrap-4.2.1-dist/css/bootstrap-reboot.css">
+  	<link rel="stylesheet" href="bootstrap-4.2.1-dist/css/bootstrap.css">
+  	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+  	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  </head>
+  <body>
+    <div class="container" >
+      <h1>Форма Регистрация</h1>
+      <form id="mailForm" class="" action="registration.php" method="get">
+        <input type="email" id ="email" name="email" placeholder="Email" class="form-control" value=""><br>
+        <input type="text" id="name" name="name" placeholder="Введите имя" class="form-control" value=""><br>
+        <input type="phone" id="phone" name="phone" placeholder="Телефон" class="form-control" value=""><br>
+        <input type="password" id="password" name="password" placeholder="Введите пароль" class="form-control" value=""><br>
+        <input type="password" id="password" name="password" placeholder="Введите пароль повторно" class="form-control" value=""><br>
+
+        <button type="button" id="sendMail" name="button" class="btn btn-success">Отправить</button><br>
+        <button formaction="registration.php" > Регистрация </button>
+      </form>
+      <div id="errorMess"></div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="js\formMail.js"></script>
+  </body>
+</html>
